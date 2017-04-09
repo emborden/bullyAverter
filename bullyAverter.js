57f4f5102c5261fbadb394d7b5945a98c4efda70
emborden <ethanmborden@gmail.com>
2017-04-08 20:45:01 -0500

    manifest now effects all urls and the .js checks for specific sites

--- bullyAverter.js
+++ bullyAverter.js
@@ -1,1 +1,18 @@
-document.body.style.border = "5px solid red";
+//manifest is functional for most urls issues with youtube resolve later 
+if(window.location.href.indexOf("facebook.com")>=0)//This does work 
+{
+	//The current website is face book
+	document.body.style.border = "5px solid red";
+}
+else if(location.hostname.indexOf("twitter.com")>=0)
+{
+	//The current website is twitter
+}
+else if(location.hostname.indexOf("reddit.com")>=0)
+{
+	//current site is reddit
+}
+else if(location.hostname.indexOf("youtube.com")>=0)
+{
+	//site is youtube
+}
