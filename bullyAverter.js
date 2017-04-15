20e4d7c02d66849fe3fa375f1cdfe46485419d2a
emborden <ethanmborden@gmail.com>
2017-04-15 09:25:39 -0500

    test

--- bullyAverter.js
+++ bullyAverter.js
@@ -4,15 +4,15 @@ if(window.location.href.indexOf("facebook.com")>=0)//This does work
 	//The current website is face book
 	document.body.style.border = "5px solid red";
 }
-else if(location.hostname.indexOf("twitter.com")>=0)
+else if(window.location.href.indexOf("twitter.com")>=0)
 {
 	//The current website is twitter
 }
-else if(location.hostname.indexOf("reddit.com")>=0)
+else if(window.location.href.indexOf("reddit.com")>=0)
 {
 	//current site is reddit
 }
-else if(location.hostname.indexOf("youtube.com")>=0)
+else if(window.location.href.indexOf("youtube.com")>=0)
 {
 	//site is youtube
 }
