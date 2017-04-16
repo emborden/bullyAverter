b55bb5c15d49742eb7be2d05d8b62848720ab390
emborden <ethanmborden@gmail.com>
2017-04-16 12:40:25 -0500

    final commit before presentation

--- bullyAverter.js
+++ bullyAverter.js
@@ -1,40 +1,80 @@
-//DO NOT DELETE FROM HERE 
-function Person(user,ulink,comment){
-	this.userName = user;
-	this.userLink = ulink;
-	this.comment = comment;
-}
-if(window.location.href.indexOf("facebook.com")>=0)
-{
-	//facebook
-	var communityInteraction =document.getElementsByClassName("hidden_elem");
-	var comsize = communityInteraction.length;
-	var i =0;
-	while(i<comsize)
+
+	$(document).ready(function ()
 	{
-	if(communityInteraction[i].getElementByTagName("im").getAttribute("aria-label")!=null&&communityInteraction[i].getElementByTagName("p")!=null){
-	var userName =communityInteraction[i].getElementsByTagName("im").getAttribute("aria-label");//TODO write code to grab that
-	var refholder = communityInteraction[i].getElementsByTagName("a");
+		
+		if(window.location.href.indexOf("facebook.com")>=0)
+		{
+			var last_known_scroll_position = 0;
+			var ticking = false;
+			var holder = new CommentLoader();
+			var c1 = document.getElementsByTagName("P").item(0);
+			var c2 = document.getElementsByTagName("P").item(1);
+			console.log(c1.innerHTML);
+			console.log(c2.innerHTML);
+			window.addEventListener('scroll', function(e) {
+				last_known_scroll_position = window.scrollY;
+				if (!ticking) {
+					window.requestAnimationFrame(function() 
+					{
+						holder.loadComments(last_known_scroll_position);
+						ticking = false;
+						});
+  }
+  ticking = true;
+});
+		}
+		else if(window.location.href.indexOf("twitter.com")>=0)twitterHandler();
+	});
+function CommentLoader()
+{
+	this.count =2;
+	this.loadComments =function(scroll_pos) 
+	{
+		var c1 = document.getElementsByTagName("P").item(this.count);
+		this.count++;
+		var c2 = document.getElementsByTagName("P").item(this.count);
+		this.count++;
+	}
+}
+/*
+function facebookHandler()
+{	
+	//facebook
+	var tags =document.getElementsByClassName("hidden_elem");
+	var people =[].forEach.call(tags,function(input){
+	var incremeneter=0;
+	var imtag = input.getElementByTagName("im");
+	var ptag = input.getElementByTagName("p");
+	while(incremeneter<imtag.length&&incremeneter<ptag)
+	{
+	if(imtag.item(incremeneter).getAttribute("aria-label")!=null&&ptag.item(incremeneter)!=null)
+	{
+	var userName =imtag.item(incremeneter).getAttribute("aria-label");//TODO write code to grab that
+	var refholder = input.getElementsByTagName("a");
 	var k=0;
 	var userPage;
 	var done =true
 	while(k<refholder.length&&done)
 	{
-	var temp = refholder[k].getAttribute("href");
+	var temp = refholder.item(k).getAttribute("href");
 	if(temp.substring(0,24)==="https://www.facebook.com"&&temp.substring(temp.length-21,temp.length)!="NEWSFEED&amp;fref=nf"){userPage=temp;done=false;}
 	k++;
 	}
-	var comment = communityInteraction[i].getElementsByTagName("p").innerHTML;//TODO write code to do this too 
-	i++;
+	var comment = ptag.item(incremeneter).innerHTML;//TODO write code to do this too 
 	var dossier = new Person(userName,userPage,comment);
+	return person;
 	}
 	}
+	return null;
+});
+	document.body.style.border = "5px solid red";
+	return people;
 }
+*/
 // TO HERE MAKES TONS OF MERGE CONFLICTS also you need to test what website you are on this extension works on all urls the only way to filter through them is with the
 //conditional statement above 
-/*
-else if(window.location.href.indexOf("twitter.com")>=0){
-import System.IO;
+function twitterHandler(){
+//import System.IO;
 var filePath = "/defaultBlacklist.txt";
 	//The current website is twitter
 	var elements = document.querySelectorAll('div.tweet.js-stream-tweet.js-actionable-tweet.js-profile-popup-actionable.original-tweet.js-original-tweet.my-tweet');
@@ -72,13 +112,13 @@ var filePath = "/defaultBlacklist.txt";
 	}
 
 var parents = document.querySelectorAll(".my-tweet");
-for(int i = 0; i < parents.length; i++){
-	var tweetText = parents[i].querySelectorAll(".tweet-text");
-	tweetText.innerHTML = tweetText.innerHTML.replace(/\bself-harm\b/g,'<a href="http://www.nami.org/Learn-More/Mental-Health-Conditions/Related-Conditions/Self-harm">self-harm</a>');
-}
-}
-*/
+//for(int i = 0; i < parents.length; i++){
+	//var tweetText = parents[i].querySelectorAll(".tweet-text");
+//	tweetText.innerHTML = tweetText.innerHTML.replace(/\bself-harm\b/g,'<a href="http://www.nami.org/Learn-More/Mental-Health-Conditions/Related-Conditions/Self-harm">self-harm</a>');
+//}
+
 
 document.body.innerHTML = document.body.innerHTML.replace(/\bself-harm\b/g,'<a href="http://www.nami.org/Learn-More/Mental-Health-Conditions/Related-Conditions/Self-harm">self-harm</a>');
 document.body.innerHTML = document.body.innerHTML.replace(/\bautism\b/g,'<a href="https://www.autismspeaks.org/what-autism">self-harm</a>');
 document.body.innerHTML = document.body.innerHTML.replace(/\bsuicide\b/g,'<a href="http://www.nami.org/Learn-More/Mental-Health-Conditions/Related-Conditions/Self-harm">self-harm</a>');
+}
